64d4cb016b776dc37573809704f82955e6c99302
saulorodriguesm <saulorodriguesm@gmail.com>
2021-02-23 07:59:03 -0300

    first server response

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
-const express = require ('express');
+const express = require('express');
 
 const app = express();
 
+app.get('/projects', (request, response) => {
+    return response.send('Nodezão brabo');
+})
 
 app.listen(3333)
