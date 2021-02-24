02d817530216c65db5884504f5dd1dc35581e6e5
saulorodriguesm <saulorodriguesm@gmail.com>
2021-02-24 07:47:25 -0300

    learning params types

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,35 +2,48 @@ const express = require('express');
 
 const app = express();
 
-app.get('/methods', (request, response) => {
+app.use(express.json());
+
+app.get('/projects', (request, response) => {
+    const { title, owner} = request.query;
+
+    console.log(title)
+    
     return response.json([
         'Luan',
         'Mateus Vital'
     ]);
-})
+});
 
 // Métodos POST, PUT/Path e Delete não são acessiveis via browser
 
-app.post('/methods', (request, response) => {
+app.post('/projects', (request, response) => {
+    const body = request.body;
+
+    console.log(body)
+
     return response.json([
         'Otero',
         'Jô'
     ]);
-})
+});
 
-app.put('/methods/:id', (request, response) => {
+app.put('/projects/:id', (request, response) => {
+    const params = request.params;
+
+    console.log(params)
     return response.json([
         'Araos',
         'Cantillo'
     ]);
-})
+});
 
-app.delete('/methods/:id', (request, response) => {
+app.delete('/projects/:id', (request, response) => {
     return response.json([
         'Araos',
         'Cantillo'
     ]);
-})
+});
 
 app.listen(3333 , () => {
     console.log('Back-end started');
