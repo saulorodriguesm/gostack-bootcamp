af8116eeb0a41da5385fcb173004aac3af24fa50
saulorodriguesm <saulorodriguesm@gmail.com>
2021-02-24 06:58:01 -0300

    an example to each http method using node was created

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.get('/projects', (request, response) => {
-    return response.json({ message: 'Nodezão brabo'});
+app.get('/methods', (request, response) => {
+    return response.json([
+        'Luan',
+        'Mateus Vital'
+    ]);
+})
+
+// Métodos POST, PUT/Path e Delete não são acessiveis via browser
+
+app.post('/methods', (request, response) => {
+    return response.json([
+        'Otero',
+        'Jô'
+    ]);
+})
+
+app.put('/methods/:id', (request, response) => {
+    return response.json([
+        'Araos',
+        'Cantillo'
+    ]);
+})
+
+app.delete('/methods/:id', (request, response) => {
+    return response.json([
+        'Araos',
+        'Cantillo'
+    ]);
 })
 
 app.listen(3333 , () => {
